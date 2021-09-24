f64f12e4807985543d11513f6b02d196969dd518
jefripunza <35704193+jefripunza@users.noreply.github.com>
2021-09-24 16:20:28 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,8 @@
-// const express = require("express");
-// const app = express()
-
-// const helmet = require('helmet');
-
-// const countrycity = require("./countrycity");
-// const port = process.env.PORT || 8080
-
-// // Middleware
-// app.use(helmet())
-
-// const http = require('http');
-// const webserver = http.createServer(app)
-// const httpServer = webserver.listen(port,)
-
-
-
 const app = require('./app');
 
+const host = '0.0.0.0';
 const port = process.env.PORT || 5000;
-app.listen(port, () => {
-  console.log(`Listening on port:${port}`);
+
+app.listen(port, host, () => {
+    console.log("Express server listening on port %d in %s mode", port, app.settings.env);
 });
